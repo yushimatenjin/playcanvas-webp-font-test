113c62e96c267996eb8a89035ad9858de1f88721
yushimatenjin <39250588+yushimatenjin@users.noreply.github.com>
2023-04-12 17:55:30 +0900

    initial commit

---
+++ __settings__webp.js
@@ -0,0 +1,21 @@
+window.ASSET_PREFIX = "";
+window.SCRIPT_PREFIX = "";
+window.SCENE_PATH = "1716496.json";
+window.CONTEXT_OPTIONS = {
+    'antialias': true,
+    'alpha': false,
+    'preserveDrawingBuffer': false,
+    'preferWebGl2': true,
+    'powerPreference': "high-performance"
+};
+window.SCRIPTS = [  ];
+window.CONFIG_FILENAME = "config_webp.json";
+window.INPUT_SETTINGS = {
+    useKeyboard: true,
+    useMouse: true,
+    useGamepads: false,
+    useTouch: true
+};
+pc.script.legacy = false;
+window.PRELOAD_MODULES = [
+];
